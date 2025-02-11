dba341fdc450b4e546dc41e67d815656e45b2abc
Joakim Bohlin <joakim.bohlin@chalmers.se>
2025-02-11 12:11:23 +0100

    Add rsg code and UI

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# redshift-search-graphs
-Efficient analysis of bandwidth coverage and spectral lines for finding z-specs 
+
+# Redshift search graphs
+Efficient analysis of bandwidth coverage and spectral lines for finding z-specs
+
+Original code by Tom Bakx: https://github.com/tjlcbakx/redshift-search-graphs
+
+User interface by Joakim Bohlin, Chalmers Infravis.
+
+## Try it now
+Simply go to https://akodiat.github.io/redshift-search-graphs to try the app yourself.
+
+## Running locally
+To run it locally, you need to start a static HTTP server in this directory. You can easily do that with the following Python command ([or any of these other options](https://gist.github.com/willurd/5720255))
+
+```sh
+python -m http.server 8000
+```
+
+Then, you can go to [localhost:8000](HTTP:/localhost:8000) (or whatever port you used above) to view the app.
+
+
+### Pyodide
+[Pyodide](https://pyodide.org/) is a Python distribution that can run in the browser (through WebAssembly), allowing you to call Python code from JavaScript.
---
+++ main.js
@@ -0,0 +1,66 @@
+// Get reference to UI elements
+const loadingIndicator = document.getElementById("loadingIndicator");
+const inputs = document.getElementById("inputs");
+const plotButton = document.getElementById("plotButton");
+
+
+document.pyodideMplTarget = document.getElementById("plotContainer");
+
+function getValue(id) {
+    return document.getElementById(id).value;
+}
+
+function getBool(id) {
+    return document.getElementById(id).checked;
+}
+
+function parseFloatList(text) {
+    return text.split(",").map(s=>parseFloat(s));
+}
+
+window.addEventListener("load", () => {
+    plot();
+});
+
+async function plot() {
+    // Setup pyodide
+    // eslint-disable-next-line no-undef
+    let pyodide = await loadPyodide();
+
+    // Load numpy
+    await pyodide.loadPackage("micropip");
+    const micropip = pyodide.pyimport("micropip");
+    await micropip.install("numpy");
+    await micropip.install("matplotlib");
+
+    // Downloading a single file
+    await pyodide.runPythonAsync(`
+        from pyodide.http import pyfetch
+        response = await pyfetch("./rsg.py")
+        with open("rsg.py", "wb") as f:
+            f.write(await response.bytes())
+    `);
+    const rsg = pyodide.pyimport("rsg");
+
+    // Remove loading indicator and show inputs
+    loadingIndicator.hidden = true;
+    inputs.hidden = false;
+
+    plotButton.onclick = () => {
+        rsg.RSGplot(
+            parseFloatList(getValue("filter_down")),
+            parseFloatList(getValue("filter_up")),
+            parseFloat(getValue("z_phot")),
+            parseFloatList(getValue("sl_freq_obs")),
+            parseFloat(getValue("figSizeX")),
+            parseFloat(getValue("figSizeY")),
+            parseFloat(getValue("redshift_down")),
+            parseFloat(getValue("redshift_up")),
+            getValue("single_line_colour"),
+            getValue("multi_line_colour"),
+            getBool("LSBUSB"),
+            parseFloat(getValue("nr_of_CO_lines")),
+            parseFloat(getValue("dzUncertainty"))
+        );
+    };
+}
